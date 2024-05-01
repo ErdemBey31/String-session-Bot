6314959e6b5e6aa599a97314710efa8741bb8d9d
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2024-05-02 00:02:05 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,10 +4,10 @@ ENVIRONMENT = False
 
 if ENVIRONMENT:
     try:
-        API_ID = 94575
+        API_ID = 24588661
     except ValueError:
         raise Exception("Your API_ID is not a valid integer.")
-    API_HASH = "a3406de8d171bb422bb6ddf3bbd800e2"
+    API_HASH = "332058c74190c9a3739f43676f3a21e0"
     BOT_TOKEN = "7189521804:AAH8B6Yv92I-1N2m4wIsLDMjS63IvZJGDDc"
     DATABASE_URL = os.environ.get('DATABASE_URL', None)
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
@@ -17,8 +17,8 @@ if ENVIRONMENT:
         MUST_JOIN = MUST_JOIN.replace("@", "")
 else:
     # Fill the Values
-    API_ID = 94575
-    API_HASH = "a3406de8d171bb422bb6ddf3bbd800e2"
+    API_ID = 24588661
+    API_HASH = "332058c74190c9a3739f43676f3a21e0"
     BOT_TOKEN = "7189521804:AAH8B6Yv92I-1N2m4wIsLDMjS63IvZJGDDc"
     DATABASE_URL = "postgresql://postgre_9kfd_user:15RVHcUF8SPQTBIstGpZMb5XqQ6x3Nxi@dpg-cop4js779t8c7382lcc0-a.oregon-postgres.render.com/postgre_9kfd"
     DATABASE_URL = DATABASE_URL
