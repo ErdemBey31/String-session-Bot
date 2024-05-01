6e30525137274db0d406c541168b4aa37435c78c
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2024-05-01 16:30:42 +0300

    Update Data.py

--- Data.py
+++ Data.py
@@ -4,61 +4,46 @@ from pyrogram.types import InlineKeyboardButton
 class Data:
     # Start Message
     START = """
-Hello {}
-
-If you don't trust this bot,
-1) don't read this message
-2) block bot or delete chat
-
-This Bot Works To Help You Get Session String Via Bot. Recommendations If You Want To Take String Use Another Account,
-Managed By @zenfrans
+Merhaba {}, bu bot ile ilk yazan botunu kurabilirsin.
     """
 
     # Home Button
     home_buttons = [
-        [InlineKeyboardButton("Start Generating Session", callback_data="generate")],
-        [InlineKeyboardButton(text="Home", callback_data="home")]
+        [InlineKeyboardButton("Botu Kur", callback_data="generate")],
+        [InlineKeyboardButton(text="Başa Dön", callback_data="home")]
     ]
 
     generate_button = [
-        [InlineKeyboardButton("Start Generating Session", callback_data="generate")]
+        [InlineKeyboardButton("Botu Kur", callback_data="generate")]
     ]
 
     # Rest Buttons
     buttons = [
-        [InlineKeyboardButton("Start Generating Session", callback_data="generate")],
-        [InlineKeyboardButton("Maintaned By", url="https://t.me/zenfrans")],
+        [InlineKeyboardButton("Botu Kur", callback_data="generate")],
+        [InlineKeyboardButton("Sahip", url="https://t.me/Anonymousss_TR")],
         [
-            InlineKeyboardButton("How to use me", callback_data="help"),
-            InlineKeyboardButton("About", callback_data="about")
-      ],
-        [InlineKeyboardButton("Other bot info", url="https://t.me/nbzoning")],
+            InlineKeyboardButton("Nasıl kullanılır", callback_data="help"),
+            InlineKeyboardButton("Hakkında", callback_data="about")
+      ]
+    
     ]
 
 
     # Help Message
     HELP = """
-✨ **Available Commands** ✨
+✨ **Mevcut komutlar** ✨
 
-/about - About this bot
-/help - How to use this bot
-/start - Start Bot
-/generate - Start Generating Session
-/cancel - Cancel process
-/restart - Restart process
+/about - Bot Hakkında Bilgi
+/help - Bot Hakkında Yardım
+/start - Botu Başlatır
+/generate - İlk Yazan Botunu Kurar
+/cancel - Kurulumu İptal Eder
+/restart - Botu Yeniden Başlatır
 """
 
     # About Message
     ABOUT = """
-**About This Bot** 
+**Bot Hakkında** 
 
-A telegram bot to retrieve pyrograms and telethon string sessions by @nbzoning
-
-Group Support : [Gabung](https://t.me/OkaeriUserbot)
-
-Framework : [Pyrogram](docs.pyrogram.org)
-
-Language : [Python](www.python.org)
-
-Developer : @zenfrans
+Ücretsiz bir şekilde ilk yazan botu kurar. Bot **otomatik dm özelliği**nede sahiptir.
     """
